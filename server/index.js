43e84ff4a53298f6f2d0a5fce0b09a415681a067
shubhamrathod0015 <shubhamrathod664@gmail.com>
2024-12-24 22:10:31 +0530

    Build the Login and Registration bussiness login in backend file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectDB from "./database/db.js";
-
+import userRoute from "./routes/user.route.js";
 dotenv.config({});
 
 // call database connection here
@@ -10,7 +12,24 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
- 
+
+// default middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:8080",
+    credentials:true
+}))
+// apis
+app.use("/api/v1/user", userRoute);
+
+
+app.get("/home", (_, res) => {
+    res.status(200).json({
+        success:true,
+        message:"Hello I am coming from backed"
+    })
+});
 app.listen(PORT, () => {
     console.log(`Server listen at port ${PORT}`);
 })
